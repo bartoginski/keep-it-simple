3ec9686ddb5701e2ed729d6b31ea80f8592a0807
Bartosz Ogiński <bartoginski@outlook.com>
2022-02-20 12:50:29 +0100

    feat: deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:bartoginski/keep-it-simple.git main:gh-pages
+git push -f git@github.com:bartoginski/keep-it-simple.git master:gh-pages
 
 
 cd -
