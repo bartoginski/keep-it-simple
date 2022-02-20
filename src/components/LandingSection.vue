e0217d4dbe1d7c55bb43fe7579b596a5970a16f7
Bartosz Ogiński <bartoginski@outlook.com>
2022-02-20 13:08:33 +0100

    fix: responsive landing add: new hero image

--- src/components/LandingSection.vue
+++ src/components/LandingSection.vue
@@ -4,6 +4,20 @@
       <a href="#about" class="landing__nav_link">Why?</a>
       <a href="#contact" class="landing__nav_link">Contact</a>
     </nav>
+    <!-- mobile -->
+    <section class="landing__main landing__main--mobile">
+      <div class="landing__content">
+        <h1 class="landing__title">Keep it Simple</h1>
+        <img
+          class="landing__image"
+          src="../assets/landing.svg"
+          alt="landing page image"
+        />
+        <p class="landing__text">Let us create website for You.</p>
+        <button class="btn btn--primary">Lets start</button>
+      </div>
+    </section>
+    <!-- desktop -->
     <section class="landing__main">
       <div class="landing__content">
         <h1 class="landing__title">Keep it Simple</h1>
@@ -39,9 +53,26 @@
 
   &__main {
     height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    display: none;
+    @media screen and (min-width: 740px) {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    &--mobile {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      @media screen and (min-width: 740px) {
+        display: none;
+      }
+      img {
+        width: 100%;
+        max-width: 600px;
+        margin-bottom: 10px;
+      }
+    }
   }
 
   &__image {
