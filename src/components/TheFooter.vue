aa8969d17eeb93e4f37c1711bed8a9d61eb10b32
Bartosz Ogiński <bartoginski@outlook.com>
2022-02-20 11:21:27 +0100

    feat: create footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="footer">
+    <div class="container"><span>Made by @bartoginski - 2022</span></div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables.scss";
+
+.footer {
+  padding: 30px 0;
+  text-align: center;
+  // background: $color-brown;
+}
+</style>
